951976b4f52deb9709b4240da59b306630cffb88
valuevstepan96@gmail.com <valuevstepan96@gmail.com>
2025-05-13 00:09:19 +0300

    Add default-constructor, copy-constructor, constructions with arguments, destructor in Knight class

--- KnightOOPHomeworkProject/Knight.h
+++ KnightOOPHomeworkProject/Knight.h
@@ -10,23 +10,85 @@ public:
 	int armor;
 	int health;
 	int age;
-	double sword_damage;
-	double hand_damage;
+	double swordDamage;
+	double handDamage;
 
-
-	string getString()
+	Knight()
 	{
-		string s = "Sword: " + sword;
-		s += ", is alive: ";
-		s += (alive ? "yes" : "no")
-			", armor: " + armor
-			+ ", health: " + health
-			+ ", sword damage: " + sword_damage + " hp"
-			+ ", hand damage: " + hand_damage + " hp"
-			+ ", age:" + to_string(age) + " years";
-
-		
-
-		return s;
+		sword = "gladius";
+		alive = true;
+		armor = 50;
+		health = 100;
+		age = 35;
+		swordDamage = 40.5;
+		handDamage = 15;
 	}
-}; //m
+
+	Knight(string longsword)
+	{
+		sword = longsword;
+		alive = true;
+		armor = 60;
+		health = 120;
+		age = 46;
+		swordDamage = 73;
+		handDamage = 24;
+	}
+
+	Knight(string katana, int a)
+	{
+		sword = katana;
+		alive = true;
+		armor = 60;
+		health = 125;
+		age = a;
+		swordDamage = 65;
+		handDamage = 30;
+	}
+
+	Knight(string odachi, bool al, int arm, int hp, int a, double sw_dmg, double h_dmg)
+	{
+		sword = odachi;
+		alive = al;
+		armor = arm;
+		health = hp;
+		age = a;
+		swordDamage = sw_dmg;
+		handDamage = h_dmg;
+	}
+
+	Knight(const Knight& knight)
+	{
+		sword = knight.sword;
+		alive = knight.alive;
+		armor = knight.armor;
+		health = knight.health;
+		age = knight.age;
+		swordDamage = knight.swordDamage;
+		handDamage = knight.handDamage;
+	}
+
+	~Knight()
+	{
+
+	}
+
+	
+
+	string toString() {
+		{
+			string s = "Sword: " + sword;
+			s += ", is alive: ";
+			s += (alive ? "yes" : "no")
+				", armor: " + armor
+				+ ", health: " + health
+				+ ", sword damage: " + sword_damage + " hp"
+				+ ", hand damage: " + hand_damage + " hp"
+				+ ", age:" + to_string(age) + " years";
+
+
+
+			return s;
+		}
+	}
+}; 
--- KnightOOPHomeworkProject/main.cpp
+++ KnightOOPHomeworkProject/main.cpp
@@ -2,33 +2,20 @@
 
 int main()
 {
-	Knight k1, k2;
-
-	k1.alive = true;
-	k1.sword = "epic";
-	k1.health = 100;
-	k1.armor = 70;
-	k1.age = 36;
-	k1.sword_damage = 38;
-	k1.hand_damage = 16;
+	Knight k1;
+	Knight k2("gladius");
+	Knight k3("katana", 125);
+	Knight k4("longsword", true, 60, 120, 46, 73, 24);
+	Knight k5(k4);
 
 
-	k2.alive = true;
-	k2.sword = "legendary";
-	k2.health = 120;
-	k2.armor = 85;
-	k2.age = 47;
-	k2.sword_damage = 50;
-	k2.hand_damage = 27;
+	cout << k1.toString() << endl;
+	cout << k2.toString() << endl;
+	cout << k3.toString() << endl;
+	cout << k4.toString() << endl;
+	cout << k5.toString() << endl;
 
-	cout << "Before: " << endl;
-	cout << k1.getString() << endl;
-	cout << k2.getString() << endl;
 
-	cout << "After: " << endl;
-	cout << k1.getString() << endl;
-	cout << k2.getString() << endl;
-	
-
+	return 0;
 	
 }
